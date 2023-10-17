e5887c941b48021eb024213638a4773455e613ed
maxinewu5 <maxinewu5@gmail.com>
2023-10-16 19:06:39 -0700

    fix file path in data_utils

--- data_utils.py
+++ data_utils.py
@@ -10,13 +10,12 @@ class Dataset(torch.utils.data.Dataset):
         # Store the folder path and desired image size for later use
         self.image_folder_path = image_folder_path
         self.desired_image_size = desired_image_size
- 
 
     def __getitem__(self, i):
         # return the ith image as a tensor
         
         allImages = [f for f in os.listdir(self.image_folder_path) if os.path.isfile(os.path.join(self.image_folder_path, f))] 
-        img = Image.open(allImages[i])
+        img = Image.open(self.image_folder_path + "/" + allImages[i])
         
         convert_tensor = transforms.ToTensor()
         convert_tensor(img)
